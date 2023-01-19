15f655003b90877767925b0f59a55771291c254d
hector silva <hectorsf21@gmail.com>
2023-01-19 17:55:15 +0100

    responsive portada

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,12 @@ export default function Index(){
     push("/aplication")
   }
   // CONSOLE
+  
   return(
   <>
     <div>
       <form className='flex mt-40 justify-center'>
-        <div className='flex flex-col w-3/12 px-7 py-7 border border-slate-400 rounded'>
+        <div className='flex flex-col w-12/12 sm:w-3/12 px-7 py-7 border border-slate-400 rounded'>
           <p className='mb-3 text-gray-800'>USUARIO</p>
           <input  className='border border-slate-400 h-8 mb-5 outline-none' type='text' name='usuario' />
           <p className='mb-3 text-gray-800'>CONTRASEÑA</p>
